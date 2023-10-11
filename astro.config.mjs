5bf7b376411865da44086046602cde0d1fc59bcc
Riximus <riximuslol@gmail.com>
2023-10-11 22:36:11 +0200

    :hammer: small adjustments and cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [],
   output: "server",
   adapter: vercel()
 });
